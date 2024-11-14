7c0bba917bf3c06b99c09cddd57053f1032c56b8
aljabri00056 <62200195+aljabri00056@users.noreply.github.com>
2024-11-14 07:23:01 +0000

    Deploying to gh-pages from @ aljabri00056/paperback-sources@1c32d064d373299085223bb18106316c66bbde8f 🚀

--- AJ/ComicK/index.js
+++ AJ/ComicK/index.js
@@ -377,7 +377,7 @@ const ComicKSettings_1 = require("./ComicKSettings");
 const ComicKParser_1 = require("./ComicKParser");
 const ComicKHelper_1 = require("./ComicKHelper");
 const BASE_URL = "https://comick.io";
-const API_BASE_URL = "https://api.comick.io";
+const API_BASE_URL = "https://api.comick.fun";
 exports.ComicKInfo = {
     name: "ComicK",
     icon: "icon.png",
